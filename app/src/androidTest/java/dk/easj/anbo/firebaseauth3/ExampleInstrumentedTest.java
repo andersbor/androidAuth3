ece0038c911aa590878a5afa829552adf5a356ae
Anders Børjesson <anbo@zealand.dk>
2021-03-20 19:49:26 +0100

    UI test incl sleep + closeSofKeyboard

--- app/src/androidTest/java/dk/easj/anbo/firebaseauth3/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/dk/easj/anbo/firebaseauth3/ExampleInstrumentedTest.java
@@ -2,12 +2,22 @@ package dk.easj.anbo.firebaseauth3;
 
 import android.content.Context;
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -17,10 +27,26 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ActivityScenarioRule rule = new ActivityScenarioRule<>(MainActivity.class);
+
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("dk.easj.anbo.firebaseauth3", appContext.getPackageName());
+    public void testLogin() {
+        onView(withText("Hello World!")).check(matches(isDisplayed()));
+        onView(withId(R.id.mainEmailEditText)).perform(typeText("andersb@gmail.com"));
+        onView(withId(R.id.mainPasswordEditText)).perform(typeText("andersb"))
+                .perform(closeSoftKeyboard());
+        onView(withId(R.id.mainLoginButton)).perform(click());
+        pause(1000);
+        onView(withId(R.id.mainMessageTextView)).check(matches(withText(("Welcome andersb@gmail.com"))));
+    }
+
+    private void pause(int millis) {
+        try {
+            Thread.sleep(millis);
+            // https://www.repeato.app/android-espresso-why-to-avoid-thread-sleep/
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
